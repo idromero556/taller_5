86185b6199db614f3eb36130e353dc8ae8be0933
dtmaciasca <dt.macias@uniandes.edu.co>
2020-02-29 00:12:11 -0500

    agrego feature register and login success

---
+++ cucumber-webdriverio/features/register.feature
@@ -0,0 +1,46 @@
+Feature: Register into losestudiantes
+    As an user I want to authenticate myself within losestudiantes website in order to rate teachers
+
+Scenario Outline: Register failed with wrong inputs
+
+    Given I go to losestudiantes home screen
+      When I open the login screen
+      And I fill with the <nombre>, <apellido>, <correo>, <universidad>, <departamento>, <programa> and <contraseña>
+      And I accept terms and conditions
+      And I try to register
+      Then I expect to see <error>
+
+      Examples:
+      | nombre  | apellido | correo          | universidad                 | departamento | programa | contraseña | error |
+      |         |     |    |     |  | |  | "Ingresa tu correo" |
+      | Leo | Lopez | l@mail.com | Universidad del Rosario | Estudios Universitarios en Ciencias Sociales | |  | "Ingresa una contraseña" |
+      | Leo | Lopez | l@mail.com | Universidad Nacional | | Maestría en Bioinformática | 1234 | "La contraseña debe ser al menos de 8 caracteres" |
+
+Scenario Outline: Register failed with wrong inputs
+
+    Given I go to losestudiantes home screen
+      When I open the login screen
+      And I fill with the <nombre>, <apellido>, <correo>, <universidad>, <departamento>, <programa> and <contraseña>
+      And I accept terms and conditions
+      And I try to register
+      Then I expect to see error input
+
+      Examples:
+      | nombre  | apellido | correo          | universidad                 | departamento | programa | contraseña |
+      |  | Lopez | l@mail.com | Universidad del Rosario | Estudios Universitarios en Ciencias Sociales | | 12345678 |
+      | Leo |  | l@mail.com | Universidad del Rosario | Estudios Universitarios en Ciencias Sociales | | 12345678 |
+      | Leo | Lopez | l@mail.com | Universidad del Rosario | | | 12345678 |
+
+Scenario Outline: Register student success
+
+    Given I go to losestudiantes home screen
+       When I open the login screen
+       And I fill with the <nombre>, <apellido>, <correo>, <universidad>, <departamento>, <programa> and <contraseña>
+       And I accept terms and conditions
+       And I try to register
+       Then I expect to register me
+
+      Examples:
+      | nombre      | apellido | correo          | universidad                      | departamento      | programa              | contraseña    |
+      | Maria       | Cardenas | mariat322@mail.com   | Universidad Nacional             |                   | Maestría en Bioinformática | 123456789  |
+      | Tati        | Lopez    | tatipruebat@mail.com   | Pontificia Universidad Javeriana | Dpto Comunicación |            | 123456789  |
---
+++ cucumber-webdriverio/features/step-definitions/index.js
@@ -0,0 +1,125 @@
+var {Given} = require('cucumber');
+var {When} = require('cucumber');
+var {Then} = require('cucumber');
+var {expect} = require('chai');
+
+Given('I go to losestudiantes home screen', () => {
+  browser.url('/');
+  if($('button=Cerrar').isDisplayed()) {
+    $('button=Cerrar').click();
+  }
+  if($('button=Cuenta').isDisplayed()) {
+    $('button=Cuenta').click()
+    $('a=Salir').click()
+  }
+});
+
+When('I open the login screen', () => {
+    const btnIngresar = $('button=Ingresar');
+    btnIngresar.waitForExist(5000);
+    btnIngresar.waitForDisplayed(5000);
+    browser.waitUntil(() => btnIngresar.isClickable());
+    btnIngresar.click();
+});
+
+When('I try to login', () => {
+  var cajaLogIn = $('.cajaLogIn');
+  cajaLogIn.waitForExist(5000);
+  cajaLogIn.$('button=Ingresar').click();
+});
+
+When('I try to register', () => {
+  var cajaLogIn = $('.cajaSignUp');
+  cajaLogIn.waitForExist(5000);
+  cajaLogIn.$('button=Registrarse').click();
+});
+
+Then('I expect to not be able to login', () => {
+  $('.aviso.alert.alert-danger').waitForDisplayed(5000);
+});
+
+/*When(/^I fill with (.*) and (.*)$/ , (email, password) => {
+  var cajaLogIn = $('.cajaLogIn');
+
+ var mailInput = cajaLogIn.$('input[name="correo"]');
+ mailInput.click();
+ mailInput.keys(email);
+
+ var passwordInput = cajaLogIn.$('input[name="password"]');
+ passwordInput.click();
+ passwordInput.keys(password)
+});*/
+
+When(/^I fill with the (.*), (.*), (.*), (.*), (.*), (.*) and (.*)$/ , (nombre, apellido, correo, universidad, departamento, programa, contraseña) => {
+   var cajaSignUp = $('.cajaSignUp');
+   cajaSignUp.waitForExist(9000);
+   cajaSignUp.waitForDisplayed(9000);
+
+   var nombreInput = cajaSignUp.$('input[name="nombre"]');
+   nombreInput.waitForExist(8000);
+   nombreInput.click();
+   nombreInput.keys(nombre);
+
+   var apellidoInput = cajaSignUp.$('input[name="apellido"]');
+   apellidoInput.click();
+   apellidoInput.keys(apellido)
+
+   var correoInput = cajaSignUp.$('input[name="correo"]');
+   correoInput.click();
+   correoInput.keys(correo)
+
+   if(programa != ''){
+     var acepta = cajaSignUp.$('label=Estudio una maestria');
+     acepta.waitForExist(5000);
+     acepta.click();
+   }
+
+   var universidadInput = cajaSignUp.$('select[name="idUniversidad"]');
+   universidadInput.click();
+   universidadInput.keys(universidad);
+   universidadInput.click();
+
+
+   if(programa != ''){
+     var programaInput = cajaSignUp.$('select[name="idPrograma"]');
+     programaInput.click();
+     programaInput.keys(programa)
+   }
+
+   if(departamento != ''){
+     var departamentoInput = cajaSignUp.$('select[name="idDepartamento"]');
+     departamentoInput.waitForExist(5000);
+     departamentoInput.click();
+     departamentoInput.keys(departamento)
+   }
+
+   var passwordInput = cajaSignUp.$('input[name="password"]');
+   passwordInput.click();
+   passwordInput.keys(contraseña)
+});
+
+When('I accept terms and conditions', () => {
+  var cajaSignUp = $('.cajaSignUp');
+
+  var acepta = cajaSignUp.$('input[name="acepta"]');
+  acepta.click();
+});
+
+Then('I expect to see {string}', error => {
+   $('.aviso.alert.alert-danger').waitForDisplayed(8000);
+   var alertText = browser.$('.aviso.alert.alert-danger').getText();
+   expect(alertText).to.include(error);
+});
+
+Then('I expect to see error input', () => {
+  $('.glyphicon.glyphicon-remove.form-control-feedback').waitForDisplayed(5000);
+});
+
+Then('I expect to see login success', () => {
+ browser.$('button=Cuenta').isDisplayed(5000);
+});
+
+Then('I expect to register me', () => {
+ var alertText = $('.sweet-alert').waitForDisplayed(8000)
+ expect(alertText).to.include("Registro exitoso");
+});
